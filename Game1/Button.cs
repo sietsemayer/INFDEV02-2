bbed2b56a5940e37723339af278d5f5fdb878d3f
William <terones@gmail.com>
2016-05-14 13:34:44 +0200

    fixed memory leak

--- Game1/Button.cs
+++ Game1/Button.cs
@@ -3,26 +3,34 @@ using Microsoft.Xna.Framework.Graphics;
 using System.Linq;
 
 namespace Game1 {
-    class Button : AbstractGuiComponent
-    {
+    class Button : AbstractGuiComponent { 
+
+
+        Vector2 location;
+        Texture2D tex;
+        Color[] data;
         public Button(int x, int y, int width, int height)
         {
             this.x = x;
             this.y = y;
             this.width = width;
             this.height = height;
-        }
-        public override void paint( Game1 parent )
-        {
-            Vector2 location = new Vector2(x, y);
-            Texture2D tex = new Texture2D(parent.GraphicsDevice, width, height);
-            Color[] data = new Color[width * height];
+             location = new Vector2(x, y);
+
+            data = new Color[width * height];
             for( int pixel = 0 ; pixel < data.Count() ; pixel++ ) {
                 //the function applies the color according to the specified pixel
                 data[pixel] = Color.White;
             }
-            tex.SetData(data);
 
+        }
+        public override void paint( Game1 parent )
+        {
+            if( tex == null ) {
+                tex = new Texture2D(parent.GraphicsDevice, width, height);
+                tex.SetData(data);
+            }
+            
             parent.spriteBatch.Draw(tex, location, Color.White);
 
         }
